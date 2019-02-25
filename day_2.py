d1d434f5ede5c89b6cb71d41026580cb05429ba7
Poosstoo <pieu91@gmail.com>
2019-02-25 14:43:05 +0530

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,17 @@
+
+i1=input()
+
+d1=input()
+
+s1=raw_input()
+
+
+# Print the sum of both integer variables on a new line.
+p=(i+i1)
+p1=(d+d1)
+p2=(s+s1)
+
+# Print the sum of the double variables on a new line.
+print(p)
+print(p1)
+print(p2)
